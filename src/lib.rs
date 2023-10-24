9e6c61eabbeb62b5b57fec8cd90b3536fb08cf7f
杜 世橋 Du Shiqiao <lucidfrontier.45@gmail.com>
2023-10-24 10:03:52 +0900

    add initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod queue;
+mod scored_item;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use queue::PriorityQueue;
+pub use scored_item::ScoredItem;
